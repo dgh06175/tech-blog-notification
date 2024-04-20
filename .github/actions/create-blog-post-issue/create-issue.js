8f714de9c44df67fc82ab5f957c8376ffa5d75f1
dgh06175 <dgh06175@gmail.com>
2024-04-20 20:55:54 +0900

    feat: 이슈 제목에 블로그 이름 표시

--- .github/actions/create-blog-post-issue/create-issue.js
+++ .github/actions/create-blog-post-issue/create-issue.js
@@ -6,7 +6,6 @@ async function run() {
     // GitHub 토큰과 포스트 데이터 가져오기
     const token = core.getInput('GITHUB_TOKEN', { required: true });
     const postData = core.getInput('POST_DATA', { required: true });
-    const posts = JSON.parse(postData);
 
     // Octokit 객체 초기화
     const octokit = github.getOctokit(token);
@@ -17,20 +16,24 @@ async function run() {
     const kstDate = new Date(currentDate.getTime() + kstOffset);
     const formattedDate = `${kstDate.getFullYear()}.${kstDate.getMonth() + 1}.${kstDate.getDate()}`; // 한국 시간에 맞게 포맷
 
-    // 포스트 데이터 파싱
-    // const posts = parsingPostData(postData)
+    // JSON 형식의 게시글 데이터 파싱
+    const posts = JSON.parse(postData);
     
-    console.log("-- postData --")
-    console.log(postData)
-    console.log("-- posts --")
+    // 블로그 이름 문자열 생성
+    const blogNames = listBlogNames(posts)
+
+    // 디버깅을 위한 출력
+    console.log("-- postData --");
+    console.log(postData);
+    console.log("-- posts --");
     console.log(posts);
 
-    const markdownPosts = formatPostsToMarkdown(posts)
+    const markdownPostsBody = formatPostsToMarkdown(posts)
 
     // 이슈 생성
     const { repo, owner } = github.context.repo;
-    const issueTitle = `새로운 블로그 게시글이 올라왔습니다. [${formattedDate}]`;
-    const issueBody = "## 새로운 블로그 게시글 목록\n\n" + markdownPosts;
+    const issueTitle = `[${formattedDate}] ${blogNames} 블로그 새로운 게시글 업로드됨!`;
+    const issueBody = "## 새로운 블로그 게시글 목록\n\n" + markdownPostsBody;
 
     const response = await octokit.rest.issues.create({
       owner,
@@ -45,32 +48,9 @@ async function run() {
   }
 }
 
-/// MARK: 파이썬에서 받아온 문자열 데이터를 객체로 바꾸는 함수.
-/// 문자열은 다음과 같이 넘어온다.
-/// f"{blog_name}||{latest_post_info['title']}||{latest_post_info['link']}"
-function parsingPostData(stringPostData) {
-  const postLines = stringPostData.trim().split('::'); // 데이터 분리 (여러개일 경우)
-    const posts = postLines.map(line => {
-      const parts = line.split('||');
-      if (parts.length === 3) { // 정확히 세 부분이 있는지 확인
-        return {
-          blogName: parts[0].trim(),
-          title: parts[1].trim(),
-          link: parts[2].trim()
-        };
-      } else {
-        console.error('Invalid post data:', line);
-        return null; // 오류 처리
-      }
-    }).filter(post => post !== null); // null이 아닌 객체만 필터링
-    return posts
-}
-
-/// MARK: posts 객체 배열을 마크다운으로 바꾸는 함수
-function formatPostsToMarkdown(posts) {
-  return posts.map(post => {
-    return `### ${post.blogName}\n\n[${post.title}](${post.link})\n`;
-  }).join('\n');
+function listBlogNames(posts) {
+  const blogNames = posts.map(post => post.blogName);
+  return blogNames.join(", ");
 }
 
 run();
