19128798cdebaa0ea75f92150a6119a734f26a82
dgh06175 <dgh06175@gmail.com>
2024-04-20 17:28:38 +0900

    feat: 날짜 추가 로직 작성

--- .github/actions/create-blog-post-issue/create-issue.js
+++ .github/actions/create-blog-post-issue/create-issue.js
@@ -10,9 +10,15 @@ async function run() {
     // Octokit 객체 초기화
     const octokit = github.getOctokit(token);
 
+    // Date 객체 생성 및 한국 시간 적용
+    const currentDate = new Date();
+    const kstOffset = 9 * 60 * 60 * 1000; // 한국 시간에 맞게 UTC 기준 시간에 9시간 추가 (밀리초 단위)
+    const kstDate = new Date(currentDate.getTime() + kstOffset);
+    const formattedDate = `${kstDate.getFullYear()}.${kstDate.getMonth() + 1}.${kstDate.getDate()}`; // 한국 시간에 맞게 포맷
+
     // 이슈 생성
     const { repo, owner } = github.context.repo;
-    const issueTitle = '새로운 블로그 게시글이 올라왔습니다!';
+    const issueTitle = `새로운 블로그 게시글이 올라왔습니다. [${formattedDate}]`;
     const issueBody = `## 새로운 블로그 게시글 목록\n\n${postData.replace(/::/g, '\n')}`;
 
     const response = await octokit.rest.issues.create({
