23961d7ef95e603a0cf05186feb45b5d8eb632f3
dgh06175 <dgh06175@gmail.com>
2024-04-22 17:11:57 +0900

    feat: txt 말고 JSON 으로 저장하도록 변경

--- scrapper/data_manager.py
+++ scrapper/data_manager.py
@@ -1,28 +1,42 @@
 import os
+import json
 
 
 class DBManager:
     def __init__(self, blog_name):
         self.blog_name = blog_name
-        self.file_path = f"pastData/{blog_name}_last_post.txt"
+        self.file_path = f"pastData/{blog_name}_link_data.json"
 
     def save_last_post_link(self, link):
-        """게시글 링크를 파일에 저장"""
+        """게시글 링크를 JSON 파일에 저장"""
+        os.makedirs(
+            os.path.dirname(self.file_path), exist_ok=True
+        )  # 디렉토리 없을경우 생성
+        data = {"link": link}
         with open(self.file_path, "w", encoding="utf-8") as file:
-            file.write(link)
+            json.dump(data, file, ensure_ascii=False, indent=4)
 
-    def load_last_post_link(self):
-        """파일에서 게시글 링크를 로드"""
-        if not os.path.exists(self.file_path):
-            return None
-        with open(self.file_path, "r", encoding="utf-8") as file:
-            return file.read().strip()
+    def link_exists(self, link):
+        """주어진 링크가 파일에 저장된 링크와 일치하는지 확인
+
+        :param link: 확인할 링크
+        :return: 링크가 파일에 존재하면 True, 그렇지 않으면 False
+        """
+
+        # 파일이 존재하지 않거나 파일 크기가 0인 경우
+        if not os.path.exists(self.file_path) or os.stat(self.file_path).st_size == 0:
+            return False
+        try:
+            with open(self.file_path, "r", encoding="utf-8") as file:
+                data = json.load(file)
+                return data.get("last_post_link") == link
+        except (json.JSONDecodeError, OSError):
+            return False
 
     def clear_all_txt_files_in_pastData(self):
         """과거링크 데이터를 모두 삭제"""
         directory = "pastData"
         for filename in os.listdir(directory):
-            if filename.endswith(".txt"):
+            if filename.endswith(".json"):
                 filepath = os.path.join(directory, filename)
-                with open(filepath, "w") as file:
-                    pass
+                os.remove(filepath)
--- scrapper/main.py
+++ scrapper/main.py
@@ -20,7 +20,9 @@ def getPostData(blogs, blog_name):
     blog_config = blogs[blog_name]  # 하드코딩된 블로그 정보 딕셔너리 불러오기
     htmlmanager = html_manager.HtmlFetcher  # HTML 클래스 인스턴스 생성
     dbmanager = data_manager.DBManager(blog_name)  # DB 클래스 인스턴스 생성
-    parser = blog_manager.BlogParser(blog_config)  # 블로그 파싱 정보 전달하며 파서 인스턴스 생성
+    parser = blog_manager.BlogParser(
+        blog_config
+    )  # 블로그 파싱 정보 전달하며 파서 인스턴스 생성
 
     html_content = htmlmanager.fetch_html_from_url(
         blog_config["url"]
@@ -28,19 +30,17 @@ def getPostData(blogs, blog_name):
 
     # dbmanager.clear_all_txt_files_in_pastData()  # DB 삭제
 
-    latest_blog_link = dbmanager.load_last_post_link()  # 로컬에 저장한 최근 링크 정보 가져오기
-
     blogKRname = blog_config["KRname"]
     title = parser.parse_title(html_content)
     link = parser.parse_link(html_content)
 
-    # 링크 불러오기 실패 or 링크 불러오기 성공, 최근 링크랑 일치 -> None 반환
-    if not link or link == latest_blog_link:
-        return None
+    # 링크 불러오기 성곰 - DB에 존재하지 않는 링크 -> 블로그 정보 반환
+    if link and not dbmanager.link_exists(link):
+        dbmanager.save_last_post_link(link=link)
+        return {"blogName": blogKRname, "title": title, "link": link}
 
-    dbmanager.save_last_post_link(link=link)
-    # 링크 불러오기 성곰 - 최근 링크 없음 or 최근 링크랑 불일치 -> 블로그 정보 반환
-    return {"blogName": blogKRname, "title": title, "link": link}
+    # 링크 불러오기 실패 or 링크 불러오기 성공했고, DB에 이미 존재하는 링크일때 -> None 반환
+    return None
 
 
 if __name__ == "__main__":
