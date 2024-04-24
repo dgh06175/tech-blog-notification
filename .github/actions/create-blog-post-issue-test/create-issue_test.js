cf6d726f49878b2f2c1c17e34767c1d128806c09
dgh06175 <dgh06175@gmail.com>
2024-04-24 22:48:07 +0900

    test: 테스트를 위한 깃허브 액션 코드 수정

--- .github/actions/create-blog-post-issue-test/create-issue_test.js
+++ .github/actions/create-blog-post-issue-test/create-issue_test.js
@@ -3,7 +3,9 @@ const { processData } = require('../create-blog-post-issue/service/postDataHandl
 
 async function run_test() {
   const postData = core.getInput('POST_DATA', { required: true });
-  const { issueTitle, issueBody } = processData(postData);
+  console.log("수신한 데이터: ")
+  console.log(postData)
+  // const { issueTitle, issueBody } = processData(postData);
 }
 
 run_test();
