9751baf7109e5406ac42f6ed34b32ae7be7ddca5
dgh06175 <dgh06175@gmail.com>
2024-06-10 11:19:53 +0900

    Rename: 서버 프로젝트 이름 변경:

---
+++ TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/TechBlogNotificationsServerApplication.java
@@ -0,0 +1,13 @@
+package com.dgh06175.techblognotificationsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBlogNotificationsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechBlogNotificationsServerApplication.class, args);
+    }
+
+}
