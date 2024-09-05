fbce816cc98f371d8edd48aa50b2a839132c3210
dgh06175 <dgh06175@gmail.com>
2024-09-05 14:02:38 +0900

    init scrapper project

---
+++ TechBlogNotificationsScrapper/src/main/java/com/dgh06175/techblognotificationsscrapper/TechBlogNotificationsScrapperApplication.java
@@ -0,0 +1,13 @@
+package com.dgh06175.techblognotificationsscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBlogNotificationsScrapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechBlogNotificationsScrapperApplication.class, args);
+    }
+
+}
