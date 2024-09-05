3b8de645b3cc737da8a441ff51491bda3a1e0708
dgh06175 <dgh06175@gmail.com>
2024-09-05 19:09:45 +0900

    feat: 게시글 업로드 날짜 순으로 정렬

--- TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/controller/TechBlogController.java
+++ TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/controller/TechBlogController.java
@@ -3,12 +3,11 @@ package com.dgh06175.techblognotificationsserver.controller;
 import com.dgh06175.techblognotificationsserver.domain.Post;
 import com.dgh06175.techblognotificationsserver.repository.AllPostRepository;
 import com.dgh06175.techblognotificationsserver.repository.PostRepository;
+import java.util.ArrayList;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +27,10 @@ public class TechBlogController {
     public List<Post> getPosts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
-        Pageable pageable = PageRequest.of(page, size);
+        List<Sort.Order> orders = new ArrayList<>();
+        orders.add(new Sort.Order(Sort.Direction.DESC, "pubDate"));
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(orders));
         return postRepository.findAll(pageable).getContent();
     }
 }
