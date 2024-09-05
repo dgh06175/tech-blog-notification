18a3959446212e645eaef2017191e48b134021e7
dgh06175 <dgh06175@gmail.com>
2024-09-05 18:26:55 +0900

    feat: 페이지네이션

--- TechBlogNotifications/TechBlogNotifications/Repository/PostManager.swift
+++ TechBlogNotifications/TechBlogNotifications/Repository/PostManager.swift
@@ -14,14 +14,17 @@ class PostManager {
         case parseError
     }
     
-    private(set) var posts: [Post] = MockData.placeHolderPosts
+    private var currentPage: Int = 0
+    
+    private(set) var posts: [Post] = []
+    //    private(set) var posts: [Post] = MockData.placeHolderPosts
     var isLoading: Bool = true
     
     // 데이터가 앱 시작시 한번만 불러와져도 되므로 init 에서 작성하고 App 시작시 초기화되도록 함
     init() {
         Task {
             do {
-                try await fetchPosts()
+                try await fetchPosts(page: currentPage)
             } catch {
                 print("\(error) 예외 발생")
             }
@@ -37,9 +40,9 @@ class PostManager {
     }
     
     // 실제 데이터 받아오기
-    private func fetchPosts() async throws {
-        guard let urlString = ProcessInfo.processInfo.environment["APIBaseURL"],
-              let url = URL(string: urlString + "posts") else {
+    private func fetchPosts(page: Int) async throws {
+        let urlString = Config.API_URL
+        guard let url = URL(string: "\(urlString)posts?page=\(page)") else {
             throw PostError.urlError
         }
         
@@ -48,48 +51,23 @@ class PostManager {
             let decoder = JSONDecoder()
             decoder.dateDecodingStrategy = .formatted(DateFormatter.iso8601Full)
             let fetchedPosts = try decoder.decode([Post].self, from: data)
-            self.posts = fetchedPosts
+            
+            //            self.posts = fetchedPosts
+            self.posts.append(contentsOf: fetchedPosts)
             self.isLoading = false
         } catch {
             print("디코딩 오류: \(error)")
             throw PostError.parseError
         }
     }
-}
-
-struct Article {
-    let id: Int
-    let content: String
-    var isFavorite: Bool = false
-}
-
-protocol ArticleRepository {
-    func loadArticles() -> [Article]
-}
-
-class Database: ArticleRepository {
-    func loadArticles() -> [Article] {
-        // 실제로는 데이터베이스에서 데이터를 불러온다.
-        return [Article(id: 3, content: "DB에서 불러온 데이터")]
-    }
-}
-
-class ArticleService {
-    private var articles: [Article]
-    private let repository: ArticleRepository
     
-    init(repository: ArticleRepository) {
-        self.articles = repository.loadArticles()
-        self.repository = repository
-    }
-    
-    // 값 반환하도록 변경
-    func calculateScore() -> Int {
-        var score = 0
-        for article in articles {
-            print(article.isFavorite ? "20추가" : "10추가")
-            score += article.isFavorite ? 20 : 10
+    // 페이지를 증가시키고 새로운 데이터를 요청하는 메서드
+    func loadNextPage() async {
+        currentPage += 1
+        do {
+            try await fetchPosts(page: currentPage)
+        } catch {
+            print("페이지를 로드하는 중 오류 발생: \(error)")
         }
-        return score
     }
 }
