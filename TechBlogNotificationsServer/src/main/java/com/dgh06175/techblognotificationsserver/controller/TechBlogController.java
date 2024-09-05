9795198837018682cdfcb66e844297b72dcd8d2e
dgh06175 <dgh06175@gmail.com>
2024-09-05 18:11:17 +0900

    feat: 페이지네이션

--- TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/controller/TechBlogController.java
+++ TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/controller/TechBlogController.java
@@ -1,6 +1,7 @@
 package com.dgh06175.techblognotificationsserver.controller;
 
 import com.dgh06175.techblognotificationsserver.domain.Post;
+import com.dgh06175.techblognotificationsserver.repository.AllPostRepository;
 import com.dgh06175.techblognotificationsserver.repository.PostRepository;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +17,18 @@ import org.springframework.web.bind.annotation.*;
 public class TechBlogController {
 
     private final PostRepository postRepository;
+    private final AllPostRepository allPostRepository;
+
+    @GetMapping("all-post")
+    public List<Post> getAllPosts() {
+        return allPostRepository.findAll();
+    }
 
     @GetMapping("posts")
-    public Page<Post> findAll(
+    public List<Post> getPosts(
             @RequestParam(defaultValue = "0") int page,
             @RequestParam(defaultValue = "20") int size) {
         Pageable pageable = PageRequest.of(page, size);
-        return postRepository.findAll(pageable);
+        return postRepository.findAll(pageable).getContent();
     }
 }
