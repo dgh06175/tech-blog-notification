27120c593a171765023c07b56955ad2725c8333f
dgh06175 <dgh06175@gmail.com>
2024-10-10 11:15:28 +0900

    style: 필요없는 import 제거

--- TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/TechBlogNotificationsServerApplication.java
+++ TechBlogNotificationsServer/src/main/java/com/dgh06175/techblognotificationsserver/TechBlogNotificationsServerApplication.java
@@ -1,10 +1,7 @@
 package com.dgh06175.techblognotificationsserver;
 
-import com.dgh06175.techblognotificationsserver.controller.TechBlogController;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
